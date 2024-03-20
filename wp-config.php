346169702ea27221d794ea529b2495a5d81e06bd
Tauseef Noor <163329746+Tauseef-Noor@users.noreply.github.com>
2024-03-20 18:13:02 +0000

    Updated Website

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'saveelkridge' );
+define( 'DB_NAME', 'content' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
